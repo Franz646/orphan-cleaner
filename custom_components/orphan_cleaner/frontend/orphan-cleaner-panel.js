17176d7e5dd9f5e3d4d3a04a901a33703b99bac9
Franz646 <Franz646@users.noreply.github.com>
2026-04-20 06:14:55 +0000

    chore: bump to 1.2.0

--- custom_components/orphan_cleaner/frontend/orphan-cleaner-panel.js
+++ custom_components/orphan_cleaner/frontend/orphan-cleaner-panel.js
@@ -509,4 +509,4 @@ class OrphanCleanerPanel extends HTMLElement {
   }
 }
 
-customElements.define("orphan-cleaner-panel-1-1-4", OrphanCleanerPanel);
+customElements.define("orphan-cleaner-panel-1-2-0", OrphanCleanerPanel);
